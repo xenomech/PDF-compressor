9bc4df9fd4f88f30fa05d6d2acc07202233e97e1
xenomech <gokulsureshkumarak@gmail.com>
2020-04-15 23:28:04 +0530

    added html with Boilerplate code

--- windows/main.js
+++ windows/main.js
@@ -8,5 +8,14 @@ const {app,BrowserWindow} = electron;
 
 let mainWindow;
 
-// ok, so now listen for the app to be ready
+// ok, so now listen for the app to be 
+
+app.on('ready',function(){
+
+    // now create window
+    
+    mainWindow = new BrowserWindow({});
+
+    // load the html file to ble displayed
+});
 
