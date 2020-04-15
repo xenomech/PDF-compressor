4d579802cf4df23dd06cd8819c700e3d24898749
xenomech <gokulsureshkumarak@gmail.com>
2020-04-15 22:22:26 +0530

    walked throug files in the directories

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import os
 
 basedir=os.path.dirname(os.path.abspath(__file__))
 orginalfiledir=os.path.join(basedir,"File to be compressed")
+
+
+for root,dirs,files in os.walk(orginalfiledir):
+    for file in files:
+        if file.endswith("pdf"):
+            path=os.path.join(root,file)
+            label=os.path.basename(root).replace(' ','-').lower()
+            print(label, path)
